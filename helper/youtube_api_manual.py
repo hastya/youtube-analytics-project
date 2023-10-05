67eddedd59f99c9e43282f93330aeba09edac15e
Anastasya Tomilova <hastya88.88@gmail.com>
2023-10-05 18:58:56 +0300

    Init

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -10,6 +10,7 @@ import isodate
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
 
+
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
 
@@ -26,7 +27,7 @@ docs: https://developers.google.com/youtube/v3/docs/channels/list
 сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
 '''
 # channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
-channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+channel_id = 'UCwHL6WHUarjGfUM_586me8w' # HighLoad Channel
 channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 printj(channel)
 
--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,24 @@
+import json
+import os
+
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key = os.getenv("YT_API_KEY")
+
+    # создать специальный объект для работы с API
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self._channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = Channel.youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
